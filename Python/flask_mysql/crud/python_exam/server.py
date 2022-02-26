01bf4bbe793bd651805f03eb18fc2520658e9001
Brandon Hart <70290947+BrandonHart1@users.noreply.github.com>
2022-02-26 00:01:36 -0500

    Delete Python/flask_mysql/crud/python_exam directory

--- Python/flask_mysql/crud/python_exam/server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-from flask_app.controllers import users
-from flask_app.controllers import shows, users
-
-
-# -----------import controllers here--------------
-
-if __name__ == '__main__':
-    app.run(debug=True)
